dfdbb977f87da261001c20c8857acd55676e6878
elton59 <eltonokoth59@gmail.com>
2024-09-09 03:58:48 +0300

    door to door delivery

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -20,7 +20,7 @@ const Banner = () => {
           <h2>Welcome to JoshMach Pharmacy</h2>
           <p>
             Your Trusted Partner in Health and Wellness<br/>
-            ABOUT US: Welcome to JoshMach Pharmacy! We take pride in being your trusted partner in health and wellness. Our mission is to provide you with top-quality pharmaceutical products and personalized care to support your well-being.
+            We provide top-quality pharmaceutical products and personalized care to support your well-being.
           </p>
           <button>SHOP NOW</button>
         </div>
@@ -28,10 +28,10 @@ const Banner = () => {
       <div className="carousel-item">
         <img src={require('../component_photos/image2.jpg')} alt="Health 2" />
         <div className="banner-text">
-        <h2>Experience Personalized Service</h2>
+          <h2>Experience Personalized Service</h2>
           <p>
             Your Health is Our Priority<br/>
-            We prioritize your health and satisfaction above all else. Our knowledgeable and friendly team is dedicated to providing you with personalized service, expert guidance, and the care you deserve.
+            Our team is dedicated to providing expert guidance and the care you deserve.
           </p>
           <button>SHOP NOW</button>
         </div>
@@ -39,12 +39,12 @@ const Banner = () => {
       <div className="carousel-item">
         <img src={require('../component_photos/image3.jpg')} alt="Health 3" />
         <div className="banner-text">
-          <h2>Experience Personalized Service</h2>
+          <h2>Door to Door Delivery</h2>
           <p>
-            Your Health is Our Priority<br/>
-             we prioritize your health and satisfaction above all else. Our knowledgeable and friendly team is dedicated to providing you with personalized service, expert guidance, and the care you deserve.
+            Convenience at Your Fingertips<br/>
+            We offer reliable door-to-door delivery services, ensuring your health essentials reach you on time.
           </p>
-          <button>SHOP NOW</button>
+          <button>ORDER NOW</button>
         </div>
       </div>
     </Carousel>
--- src/components/Products.js
+++ src/components/Products.js
@@ -4,6 +4,7 @@ import Banner from './Banner';
 import Navigation from './Navigation';
 import './Products.css';
 import ChatBot from './ChatBot';
+
 const Products = () => {
     const [products, setProducts] = useState([]);
     const navigate = useNavigate();
@@ -11,7 +12,7 @@ const Products = () => {
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                const response = await fetch('https://joshmachpharmacy-e682e263652d.herokuapp.com/api/products/all');
+                const response = await fetch('http://localhost:4000/api/products/all');
                 const data = await response.json();
                 setProducts(data);
             } catch (error) {
@@ -28,16 +29,14 @@ const Products = () => {
 
     const handleProductClick = async (id) => {
         try {
-            const response = await fetch(`https://joshmachpharmacy-e682e263652d.herokuapp.com/api/products/details?id=${id}`);
+            const response = await fetch(`http://localhost:4000/api/products/details?id=${id}`);
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
             const data = await response.json();
-            // Navigate to product details page with product data
             navigate(`/product/${id}`, { state: { product: data } });
         } catch (error) {
             console.error('Error fetching product details:', error);
-            // Handle error as needed (e.g., show error message)
         }
     };
 
@@ -48,7 +47,11 @@ const Products = () => {
             {products.map(product => (
                 <div key={product.id} className="product" onClick={() => handleProductClick(product.id)}>
                     {product.discount && <div className="sale-badge">Save {product.discount}%</div>}
-                    <img src={`https://joshmachpharmacy-e682e263652d.herokuapp.com${product.image_path}`} alt={product.product_name} />
+                    {/* Construct the full Cloudinary URL for the image */}
+                    <img 
+                        src={`https://res.cloudinary.com/dn39bkkiq/image/upload/${product.image_path}`} 
+                        alt={product.product_name} 
+                    />
                     <div className="product-info">
                         <span className="category">{product.category}</span>
                         <span className="name">{product.product_name}</span>
@@ -59,7 +62,7 @@ const Products = () => {
                     </div>
                 </div>
             ))}
-            <ChatBot/>
+            <ChatBot />
         </div>
     );
 };
